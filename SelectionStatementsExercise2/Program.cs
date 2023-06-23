9ae0f29bb12539b5c614f1a959a6afc8be68fd21
dylan wuu <dylanwuu@oulook.com>
2023-06-23 15:53:38 -0700

    exercise complete

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("what is your favorite school subject?");
+            string userInput = Console.ReadLine();
+
+            switch (userInput.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I like Math as well");
+                    break;
+                case "english":
+                    Console.WriteLine("I like English");
+                    break;
+                case "social studies":
+                    Console.WriteLine("I like Social Studies");
+                    break;
+                case "history":
+                    Console.WriteLine("I like History");
+                    break;
+                case "physical exercise":
+                    Console.WriteLine("I like PE");
+                    break;
+                default:
+                    Console.WriteLine("Never heard of it");
+                    break;
+
+            }
         }
     }
 }
